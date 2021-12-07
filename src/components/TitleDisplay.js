269a85bc58acb89de37e16b50d983bb7949ddcab
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-07 13:05:42 -0500

    lecture work-in-progress

--- src/components/TitleDisplay.js
+++ src/components/TitleDisplay.js
@@ -23,8 +23,4 @@ const mapStateToProps = (state) => {
   });
 }
 
-const mapActionsToProps = {
-  toggleEditing: toggleEditing
-}
-
-export default connect(mapStateToProps, mapActionsToProps)(TitleDisplay);
+export default connect(mapStateToProps, { toggleEditing })(TitleDisplay);
