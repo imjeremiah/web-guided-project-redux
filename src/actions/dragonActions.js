a79eec9c7ea5bbf12d88dba08b7de0b9063ae8bd
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-07 13:41:20 -0500

    lecture work-in-progress

--- src/actions/dragonActions.js
+++ src/actions/dragonActions.js
@@ -1,2 +1,7 @@
 //1. create our action constant for addMember
+export const ADD_MEMBER = "ADD_MEMBER";
+
 //2. create our action creator function for addMember
+export addMember = (name) => {
+    return({type: ADD_MEMBER, payload:{name:name, dragonStatus:true}});
+}
