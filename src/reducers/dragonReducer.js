4c05e7b7080c6462093a023fe8b043be821706b8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-07 13:18:30 -0500

    lecture work-in-progress

---
+++ src/reducers/dragonReducer.js
@@ -0,0 +1,10 @@
+//Setup State
+//1. initialize initialState
+//2. add members as a empty array
+//3. export initialState
+
+//Setup Reducer function
+//1. Create function with state and action.
+//2. create a switch statement that handles action.types
+//3. create a case for addMember
+//4. default case
